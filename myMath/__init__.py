be580f7c7b326b3ba9afee23f383b253c35b4d36
jcacabee <79921332+jcacabee@users.noreply.github.com>
2021-03-06 00:38:27 +0800

    Add files via upload

--- myMath/__init__.py
+++ myMath/__init__.py
@@ -1,5 +1,1 @@
-Python 3.9.2 (tags/v3.9.2:1a79785, Feb 19 2021, 13:44:55) [MSC v.1928 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> '''註解註解'''
-'註解註解'
->>> 
+'''註解'''
